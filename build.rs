a287c817117954de57649e12bbf1f7540f08be31
Andrew Luka <andrew.luka.ia@gmail.com>
2024-01-22 21:03:26 +0200

    updated the build process

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
+//! Build script for ccadical.
+//! This script is responsible for compiling the cadical C++ library.
+//! For more information:
+//! https://doc.rust-lang.org/cargo/reference/build-scripts.html
+//! https://doc.rust-lang.org/cargo/reference/build-script-examples.html
+
+use std::fs;
+
 fn main() -> std::io::Result<()> {
     let mut build = cc::Build::new();
     build
@@ -23,89 +31,43 @@ fn main() -> std::io::Result<()> {
         build.debug(false).opt_level(3).define("NDEBUG", None);
     }
 
-    let mut files = vec![
-        "src/ccadical.cpp",
-        "cadical/src/version.cpp",
-        "cadical/src/solver.cpp",
-        "cadical/src/internal.cpp",
-        "cadical/src/arena.cpp",
-        "cadical/src/proof.cpp",
-        "cadical/src/limit.cpp",
-        "cadical/src/options.cpp",
-        "cadical/src/stats.cpp",
-        "cadical/src/message.cpp",
-        "cadical/src/external.cpp",
-        "cadical/src/profile.cpp",
-        "cadical/src/terminal.cpp",
-        "cadical/src/clause.cpp",
-        "cadical/src/backtrack.cpp",
-        "cadical/src/phases.cpp",
-        "cadical/src/report.cpp",
-        "cadical/src/flags.cpp",
-        "cadical/src/solution.cpp",
-        "cadical/src/assume.cpp",
-        "cadical/src/queue.cpp",
-        "cadical/src/checker.cpp",
-        "cadical/src/score.cpp",
-        "cadical/src/lucky.cpp",
-        "cadical/src/propagate.cpp",
-        "cadical/src/analyze.cpp",
-        "cadical/src/ema.cpp",
-        "cadical/src/averages.cpp",
-        "cadical/src/minimize.cpp",
-        "cadical/src/extend.cpp",
-        "cadical/src/restore.cpp",
-        "cadical/src/walk.cpp",
-        "cadical/src/watch.cpp",
-        "cadical/src/decide.cpp",
-        "cadical/src/collect.cpp",
-        "cadical/src/var.cpp",
-        "cadical/src/condition.cpp",
-        "cadical/src/occs.cpp",
-        "cadical/src/subsume.cpp",
-        "cadical/src/elim.cpp",
-        "cadical/src/cover.cpp",
-        "cadical/src/block.cpp",
-        "cadical/src/backward.cpp",
-        "cadical/src/vivify.cpp",
-        "cadical/src/probe.cpp",
-        "cadical/src/decompose.cpp",
-        "cadical/src/rephase.cpp",
-        "cadical/src/reduce.cpp",
-        "cadical/src/gates.cpp",
-        "cadical/src/deduplicate.cpp",
-        "cadical/src/restart.cpp",
-        "cadical/src/ternary.cpp",
-        "cadical/src/transred.cpp",
-        "cadical/src/instantiate.cpp",
-        "cadical/src/bins.cpp",
-        "cadical/src/compact.cpp",
-        "cadical/src/contract.cpp",
-        "cadical/src/util.cpp",
-        "cadical/src/config.cpp",
-        "cadical/src/file.cpp",
-        "cadical/src/tracer.cpp",
-        "cadical/src/parse.cpp",
-        "cadical/src/format.cpp",
-        "cadical/src/constrain.cpp",
-        "cadical/src/reap.cpp",
-        "cadical/src/shrink.cpp",
-    ];
+    // create list of files to compile
+    let mut files = vec![];
 
-    if build.get_compiler().is_like_msvc() {
-        build.include(std::path::Path::new("src/msvc"));
-        files.push("src/msvc/resources.cpp");
-        files.push("src/msvc/lookahead.cpp");
-    } else {
-        files.push("cadical/src/resources.cpp");
-        files.push("cadical/src/lookahead.cpp");
+    // add interface that we added
+    files.push("src/ccadical.cpp".to_string());
+
+    // add cadical .cpp files
+    let dir_entries = fs::read_dir("cadical/src").unwrap();
+    for path in dir_entries {
+        let dir_entry = path.unwrap();
+        let path = dir_entry.path();
+        let path_str = path.to_str().unwrap().to_string();
+        if path_str.ends_with(".cpp") {
+            println!("file {}", path_str);
+            files.push(path_str);
+        }
     }
 
+    // add resources and lookahead files
+    if build.get_compiler().is_like_msvc() {
+        build.include(std::path::Path::new("src/msvc"));
+        files.push("src/msvc/resources.cpp".to_string());
+        files.push("src/msvc/lookahead.cpp".to_string());
+    } else {
+        files.push("cadical/src/resources.cpp".to_string());
+        files.push("cadical/src/lookahead.cpp".to_string());
+    }
+
+    // add files which will be compiled
     build.files(files.iter());
-    for &file in files.iter() {
+
+    // tell the compiler to recompile if any of the files changed
+    for file in files.iter() {
         println!("cargo:rerun-if-changed={file}");
     }
 
+    // compile
     build.compile("ccadical");
     Ok(())
 }
