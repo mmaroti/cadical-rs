47168248d6a14689ce254b03baee8de7713ad55b
Miklos Maroti <mmaroti@gmail.com>
2025-04-24 10:53:40 +0200

    updated to cadical 1.9.5 version

--- build.rs
+++ build.rs
@@ -23,84 +23,45 @@ fn main() -> std::io::Result<()> {
         build.debug(false).opt_level(3).define("NDEBUG", None);
     }
 
-    let mut files = vec![
-        "src/ccadical.cpp",
-        "cadical/src/version.cpp",
-        "cadical/src/solver.cpp",
-        "cadical/src/internal.cpp",
-        "cadical/src/arena.cpp",
-        "cadical/src/proof.cpp",
-        "cadical/src/limit.cpp",
-        "cadical/src/options.cpp",
-        "cadical/src/stats.cpp",
-        "cadical/src/message.cpp",
-        "cadical/src/external.cpp",
-        "cadical/src/profile.cpp",
-        "cadical/src/terminal.cpp",
-        "cadical/src/clause.cpp",
-        "cadical/src/backtrack.cpp",
-        "cadical/src/phases.cpp",
-        "cadical/src/report.cpp",
-        "cadical/src/flags.cpp",
-        "cadical/src/solution.cpp",
-        "cadical/src/assume.cpp",
-        "cadical/src/queue.cpp",
-        "cadical/src/checker.cpp",
-        "cadical/src/score.cpp",
-        "cadical/src/lucky.cpp",
-        "cadical/src/propagate.cpp",
-        "cadical/src/analyze.cpp",
-        "cadical/src/ema.cpp",
-        "cadical/src/averages.cpp",
-        "cadical/src/minimize.cpp",
-        "cadical/src/extend.cpp",
-        "cadical/src/restore.cpp",
-        "cadical/src/walk.cpp",
-        "cadical/src/watch.cpp",
-        "cadical/src/decide.cpp",
-        "cadical/src/collect.cpp",
-        "cadical/src/var.cpp",
-        "cadical/src/condition.cpp",
-        "cadical/src/occs.cpp",
-        "cadical/src/subsume.cpp",
-        "cadical/src/elim.cpp",
-        "cadical/src/cover.cpp",
-        "cadical/src/block.cpp",
-        "cadical/src/backward.cpp",
-        "cadical/src/vivify.cpp",
-        "cadical/src/probe.cpp",
-        "cadical/src/decompose.cpp",
-        "cadical/src/rephase.cpp",
-        "cadical/src/reduce.cpp",
-        "cadical/src/gates.cpp",
-        "cadical/src/deduplicate.cpp",
-        "cadical/src/restart.cpp",
-        "cadical/src/ternary.cpp",
-        "cadical/src/transred.cpp",
-        "cadical/src/instantiate.cpp",
-        "cadical/src/bins.cpp",
-        "cadical/src/compact.cpp",
-        "cadical/src/contract.cpp",
-        "cadical/src/util.cpp",
-        "cadical/src/config.cpp",
-        "cadical/src/file.cpp",
-        "cadical/src/tracer.cpp",
-        "cadical/src/parse.cpp",
-        "cadical/src/format.cpp",
+    let excluded = vec![
+        "cadical/src/resources.cpp",
+        "cadical/src/lookahead.cpp",
+        "cadical/src/ccadical.cpp",
+        "cadical/src/cadical.cpp",
+        "cadical/src/mobical.cpp",
     ];
+
+    let mut files = vec!["src/ccadical.cpp".to_string()];
+    for file in std::fs::read_dir("cadical/src/").unwrap() {
+        let file = file.unwrap().path().to_str().unwrap().to_string();
+        if file.ends_with(".cpp") && !excluded.contains(&file.as_str()) {
+            files.push(file);
+        }
+    }
 
     if build.get_compiler().is_like_msvc() {
         build.include(std::path::Path::new("src/msvc"));
-        files.push("src/msvc/resources.cpp");
-        files.push("src/msvc/lookahead.cpp");
+        files.push("src/msvc/resources.cpp".to_string());
+        files.push("src/msvc/lookahead.cpp".to_string());
     } else {
-        files.push("cadical/src/resources.cpp");
-        files.push("cadical/src/lookahead.cpp");
+        files.push("cadical/src/resources.cpp".to_string());
+        files.push("cadical/src/lookahead.cpp".to_string());
     }
 
     build.files(files.iter());
-    for &file in files.iter() {
+    for file in files.iter() {
         println!("cargo:rerun-if-changed={}", file);
+    }
+
+    println!("cargo:rerun-if-env-changed=CC");
+    println!("cargo:rerun-if-env-changed=CFLAGS");
+    println!("cargo:rerun-if-env-changed=CXX");
+    println!("cargo:rerun-if-env-changed=CXXFLAGS");
+    println!("cargo:rerun-if-env-changed=CXXSTDLIB");
+
+    // fixing errors when using clang
+    if build.get_compiler().is_like_clang() {
+        build.cpp_set_stdlib("c++");
     }
 
     build.compile("ccadical");
